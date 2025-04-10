17513c89f6dbda9530f50b927c86401541be1a1f
Karamveer Singh Oberoi <71765505+karamveer2648@users.noreply.github.com>
2025-04-11 01:02:29 +0530

    major push

--- core/core/urls.py
+++ core/core/urls.py
@@ -26,8 +26,13 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
-    # path('about/', views.about, name='about'),
-    # path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms-and-conditions/', views.terms_and_conditions, name='terms_and_conditions'),
+    path('faqs/', views.faqs, name='faqs'),
+    path('our_team/', views.team, name='our_team'),
+    
     path('scheme/<int:scheme_id>/', scheme_detail, name='scheme_detail'),
     path('add-scheme/', add_scheme, name='add_scheme'),
     path('register/', register, name='register'),
@@ -36,7 +41,10 @@ urlpatterns = [
     path('admin_user/', admin_user, name='admin_user'),
     path('logout/', logout_user, name='logout'),
     path('staff_user/', staff_user, name='staff_user'), 
-    path('activate-user/<int:user_id>/', activate_user, name='activate_user')   
+    path('activate-user/<int:user_id>/', activate_user, name='activate_user'),  
+    path('manage_scheme/', manage_scheme, name='manage_scheme'),
+    path('update_scheme/<int:scheme_id>/', update_scheme, name='update_scheme'),
+    path('delete_scheme/<int:scheme_id>/', delete_scheme, name='delete_scheme'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
